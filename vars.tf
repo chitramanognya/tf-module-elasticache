a8bb4e73cf2c0baac75dac5afec684a213d99c67
chitra <chmanu.chinu@gmail.com>
2023-04-15 09:52:45 +0000

    tf-module-elasticache

---
+++ vars.tf
@@ -0,0 +1,7 @@
+  variable "env" {}
+ variable "subnet_ids" {}
+  variable "tags" {}
+  variable "engine" {}
+  variable "engine_version" {}
+  variable "num_cache_nodes"  {}
+  variable "node_type" {}
